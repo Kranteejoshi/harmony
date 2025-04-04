5137de0e95c64234d2a5bbe276ad650fea7a35d9
Kranti Joshi <joshikrantee@gmail.com>
2025-04-04 23:29:41 +0545

    updates: response types

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -2,13 +2,13 @@ const User = require("../models/user")
 
 const getUser = async (req, res) => {
     const data = await User.find()
-    res.send(data)
-  }
+    res.json(data)
+}
 
-  const registerNewUser = async (req, res) => {
+const registerNewUser = async (req, res) => {
     req.body.password = bcrypt.hash(req.body.password, 10)
     User.create(req.body)
-    res.send('user created!')
-  }
+    res.json({msg: 'user created!'})
+}
 
-  module.exports = {getUser, registerNewUser}
+module.exports = {getUser, registerNewUser}
